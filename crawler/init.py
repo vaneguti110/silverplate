877981cb455e04fe16f909e3a5adb6fb24d8ac5f
lucasgr7 <lucasgr7@gmail.com>
2016-07-18 23:36:23 -0300

    Incluid array único, e processo de "caminhar" pelas urls limitado

--- crawler/init.py
+++ crawler/init.py
@@ -9,14 +9,34 @@ class MyHTMLParser(HTMLParser):
 	def handle_starttag(self, tag, attrs):
 		if str(tag) == 'a':
 			for attr in attrs:
-				if attr == 'href':
-					for inn in attrs:
-						if 'http' in inn:
-							print(inn)
-							self.links.append(inn)
+				for inn in attr:
+					if 'http' in inn and 'comidaereceitas.com' in inn and not 'whatsapp' in inn and not 'facebook' in inn:
+						self.push(inn)
+
+	def push(self, link):
+		for ilink in self.links:
+			if ilink == link:
+				return
+
+		self.links.append(link)
+
+
 
 #Inicialização
 response = urllib.request.urlopen('https://www.comidaereceitas.com.br/bolos/bolinho-de-chuva-pratico.html')
 html = response.read().decode('utf-8')
 parser = MyHTMLParser()
 parser.feed(html)
+acessos = 50
+i = 0
+while acessos > 0:
+	link = parser.links[i]
+	print(link)
+	print('acessos ' + str(acessos))
+	response = urllib.request.urlopen(link)
+	html = response.read().decode('utf-8')
+	parser.feed(html)
+	i += 1
+	acessos -= 1
+	
+print('links encontrados' + str(len(parser.links)))
