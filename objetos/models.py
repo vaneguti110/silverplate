5972f175ef46c8f9976bd28f76b5bdf71fe1453d
Michał Piotr Stankiewicz <ms335789@students.mimuw.edu.pl>
2016-08-26 14:32:41 +0200

    Fixed minor bugs in fields name and created tests to ensure it will work in future

--- objetos/models.py
+++ objetos/models.py
@@ -26,11 +26,10 @@ class Image(models.Model):
     url = models.CharField(max_length=500)
 
     def __str__(self):
-        return self.descricao
+        return self.description
 
 
 class Recipe(models.Model):
-    description = models.CharField(max_length=250)
     step = models.CharField(max_length=1000)
     ingredients = models.ManyToManyField(Ingredient)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -38,7 +37,7 @@ class Recipe(models.Model):
     description = models.CharField(max_length=500)
 
     def __str__(self):
-        return self.descricao
+        return self.description
 
 
 class Recipe_Ingredient(models.Model):
