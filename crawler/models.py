ebf940b0e7c977f24c5435be337993435fae0bb5
Michał Piotr Stankiewicz <ms335789@students.mimuw.edu.pl>
2016-08-26 14:13:13 +0200

    Some code refactoring (especailly docs, which were not docs)

--- crawler/models.py
+++ crawler/models.py
@@ -1,38 +1,38 @@
 from django.db import models
 
+
 # Create your models here.
-'''
-Class used to Store Ingredients of the recipes found in the crawling process
-'''
+
+
+
 class Data_Ingredient(models.Model):
-	Ingredient = models.CharField(max_length=1000)
-	Recipe = models.CharField(max_length=500)
-	Group = models.CharField(max_length=500, default='Ingredientes')
+    """"Class used to Store Ingredients of the recipes found in the crawling process"""
 
-	def __str__(self):
-		return self.Ingredient
-'''
-Class used to Store steps of the recipes found in the crawling process
-'''
+    Ingredient = models.CharField(max_length=1000)
+    Recipe = models.CharField(max_length=500)
+    Group = models.CharField(max_length=500, default='Ingredientes')
+
+    def __str__(self):
+        return self.Ingredient
+
+
 class Data_Way_Cooking(models.Model):
-	Description = models.CharField(max_length=500)
-	Recipe = models.CharField(max_length=500)
-	Group = models.CharField(max_length=500, default='Modo de Fazer')
-	
-	def __str__(self):
-		return self.Description
+    """Class used to Store steps of the recipes found in the crawling process"""
+    Description = models.CharField(max_length=500)
+    Recipe = models.CharField(max_length=500)
+    Group = models.CharField(max_length=500, default='Modo de Fazer')
 
-'''
-Class used to manipulate Ingredients found and change data to data mining and found patterns of ingredients
-'''
+    def __str__(self):
+        return self.Description
+
+
 class Ingredient_Spec(models.Model):
-	Word = models.CharField(max_length=500)
-	Count = models.IntegerField()
-	Type = models.CharField(max_length=1)
+    """Class used to manipulate Ingredients found and change data to data mining and found patterns of ingredients"""
+    Word = models.CharField(max_length=500)
+    Count = models.IntegerField()
+    Type = models.CharField(max_length=1)
 
-'''
-Model to store words to ignore from Ingredient Spec
-'''
+
 class Ignore_Words(models.Model):
-	Word = models.CharField(max_length=500)
-
+    """Model to store words to ignore from Ingredient Spec"""
+    Word = models.CharField(max_length=500)
