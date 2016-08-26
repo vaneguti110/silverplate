0661335150059b8e91074e5c10f8ec5f58aceaa7
Michał Piotr Stankiewicz <ms335789@students.mimuw.edu.pl>
2016-08-26 21:48:09 +0200

    Basic base.html and index.html templates (with no content) for main page.

--- silverplate/urls.py
+++ silverplate/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^crawl/', include('crawler.urls')),
+
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home')
 ]
