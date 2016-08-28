9cb83001a51cc8831119f9e5eb6ed42005bfe09e
Michał Piotr Stankiewicz <ms335789@students.mimuw.edu.pl>
2016-08-28 16:37:44 +0200

    style of init.py file

--- init.py
+++ init.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python
-from crawler.LinkFinder import LinkFinder
-from crawler.DataFinder import IngredientFinder
+import os
+import django
 import urllib.request
 
-import os
+from crawler.LinkFinder import LinkFinder
+from crawler.DataFinder import IngredientFinder
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "silverplate.settings")
-import django
 django.setup()
-# your imports, e.g. Django models
-from crawler.models import Data_Ingredient
 
-
-#Inicialization
+# Inicialization
 response = urllib.request.urlopen('https://www.comidaereceitas.com.br/bolos/bolinho-de-chuva-pratico.html')
 html = response.read().decode('utf-8')
 parser = LinkFinder()
@@ -22,27 +19,26 @@ parser.push('https://www.comidaereceitas.com.br/bolos/bolinho-de-chuva-pratico.h
 acessos = 1
 i = 0
 while acessos > 0:
-	link = parser.links[i]
-	print(link)
-	print('acessos ' + str(acessos))
-	response = urllib.request.urlopen(link)
-	html = response.read().decode('utf-8')
-	parser.feed(html)
-	i += 1
-	acessos -= 1
-	
+    link = parser.links[i]
+    print(link)
+    print('acessos ' + str(acessos))
+    response = urllib.request.urlopen(link)
+    html = response.read().decode('utf-8')
+    parser.feed(html)
+    i += 1
+    acessos -= 1
+
 print('links encontrados : ' + str(len(parser.links)))
 print('Will start recovering data from the site')
 
 DataParser = IngredientFinder()
 size = len(parser.links)
 for link in parser.links:
-	size -= 1
-	response = urllib.request.urlopen(link)
-	html = response.read().decode('utf-8')
-	DataParser.feed(html)
-	#print('processando o link %s, numero : %s' % (link , str(size)))
+    size -= 1
+    response = urllib.request.urlopen(link)
+    html = response.read().decode('utf-8')
+    DataParser.feed(html)
+# print('processando o link %s, numero : %s' % (link , str(size)))
 
 print('Found %s ingredients' % DataParser.ingredientes)
 print('Found %s Steps Cooking' % DataParser.passos)
-
