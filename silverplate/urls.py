7e0694c85eb4da919284fc7e6a2128e86af47bef
Michał Piotr Stankiewicz <stinger.dat@gmail.com>
2016-08-31 20:27:39 +0200

    feat(templates): half way and proposition of template

--- silverplate/urls.py
+++ silverplate/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic.base import TemplateView
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^crawl/', include('crawler.urls', namespace='crawl')),
 
-    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home')
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
+    url(r'^contact$', TemplateView.as_view(template_name='contact.html'), name='contact'),
+    url(r'^about', TemplateView.as_view(template_name='about.html'), name='about')
 ]
